c870986dd4eb8cb310619b45f5bbda9b048ed752
“krushabh-dev” <rushabhrbs@gmail.com>
2022-09-05 16:33:29 +0530

    Create Editor.jsx

---
+++ src/Component/Editor.jsx
@@ -0,0 +1,46 @@
+import React, {useEffect} from "react";
+import { Box } from "@mui/material";
+import Quill from "quill";
+import 'quill/dist/quill.snow.css'
+
+const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']                                         // remove formatting button
+  ];
+  
+
+function Editor() {
+
+    useEffect(() => {
+        var quillServer = new Quill('#editors', {
+            theme: 'snow', modules: {
+                toolbar: toolbarOptions
+              },
+          });
+    }, [])
+    
+  return (
+    <>
+    <Box>
+        <Box id="editors"></Box>
+    </Box>
+    </>
+  );
+}
+
+export default Editor;
